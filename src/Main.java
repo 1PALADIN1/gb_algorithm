715b0f25855da53e62758ad37fece9c68fbdc404
ruslan <sonikx94@yandex.ru>
2023-03-18 11:04:47 +0300

    Lesson 4

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import lesson3.List;
+import lesson4.Tree;
 
 public class Main {
     public static void main(String[] args) {
 //        lesson3();
+        lesson4();
     }
 
     private static void lesson3() {
@@ -18,5 +20,21 @@ public class Main {
 
         list.revert();
         list.print();
+    }
+
+    private static void lesson4() {
+        Tree<Integer> integerTree = new Tree<>();
+        int[] values = new int[] {
+                12, 1, 200, 90, 10, 45, 45, 42
+        };
+
+        for (int i = 0; i < values.length; i++) {
+            boolean result = integerTree.add(values[i]);
+            if (result) {
+                System.out.println(values[i] + " - successfully added!");
+            } else {
+                System.out.println(values[i] + " - already exists...");
+            }
+        }
     }
 }
